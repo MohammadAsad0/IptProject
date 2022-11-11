51675c2e100a247c9011952f669c411b8c909f01
anumf <anumf@FATIMA>
2022-11-11 15:51:28 +0500

    Add project files.

---
+++ IptProjectWeb/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using IptProjectWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IptProjectWeb.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Item> items = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://sportsequipmentapi.azurewebsites.net/api/items/");
+
+                var responseTask = client.GetAsync("getallItems");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Item>>();
+                    readTask.Wait();
+
+                    items = readTask.Result;
+                }
+                else
+                {
+                    items = Enumerable.Empty<Item>();
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact Administrator.");
+                }
+            }
+
+            return View(items);
+        }
+    }
+}
---
+++ IptProjectWeb/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IptProjectWeb.Models
+{
+    public class Item
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int PricePerItem { get; set; }
+        public int Quantity { get; set; }
+    }
+}
